5da33084f89e4719e57ac1271ccde3efaa008ce7
Hannah Heath <heathhan@pdx.edu>
2020-11-24 21:09:23 -0800

    closer to getting the api data to output

--- incidents.js
+++ incidents.js
@@ -1,4 +1,24 @@
-const url = "https://heathhan.carto.com/api/v2/sql?q=SELECT * FROM incidents_part1_part2";
+const url = "https://heathhan.carto.com/api/v2/sql?q=SELECT location_block, text_general_code, dispatch_date_time FROM incidents_part1_part2 ORDER BY dispatch_date_time";
+
+const apifeed = document.getElementById("apifeed");
+
 fetch(url)
 .then(res => res.json())
-.then((data) => console.log(data));
+.then((data) => initialize(data))
+//.then((data) => console.log(data))
+.catch((err) => console.log("Error:", err));
+
+function initialize(incidents) {
+    for(let i = 0; i < 50; ++i) {
+        var newItem = document.createElement("ol")
+        var element = incidents.rows[i];
+        var location = element.location_block;
+        var date_time = element.dispatch_date_time;
+        var event = element.text_general_code;
+        
+        console.log(location, date_time, event);
+    
+        newItem.innertext = `${location}`;
+        apifeed.appendChild(newItem);
+    };
+};
