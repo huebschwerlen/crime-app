9248a4e2ef8bcbcc82a9591306b164e87e2d1d3d
Hannah Heath <heathhan@pdx.edu>
2020-11-25 12:39:36 -0800

    API feed populating - but needs reformatting

--- incidents.js
+++ incidents.js
@@ -1,16 +1,37 @@
 const url = "https://heathhan.carto.com/api/v2/sql?q=SELECT location_block, text_general_code, dispatch_date_time FROM incidents_part1_part2 ORDER BY dispatch_date_time";
 
-const apifeed = document.getElementById("apifeed");
+const apifeed = document.getElementById("incidents");
 
-fetch(url)
-.then(res => res.json())
-.then((data) => initialize(data))
-//.then((data) => console.log(data))
-.catch((err) => console.log("Error:", err));
+
+async function postData(temp = '') {
+    const res = await fetch(temp, {
+        method: 'POST',
+        mode: 'cors',
+        cache: 'no-cache',
+        //credentials: 'same-origin',
+        headers: {
+            //'Set-Cookie': 'IP_JAR; SameSite=None',
+            'Set-Cookie': '_gid=GA1.2.261869541.1606241577; SameSite=None; Secure',
+            'Set-Cookie': '_ga=GA1.2.1250983371.1605905149; SameSite=None; Secure' 
+        },
+        //body: JSON.stringify(data)
+
+    });
+    return res.json();
+}
+
+postData(url) 
+    //fetch(url)
+    //.then(res => res.json())
+    .then(data => {initialize(data);
+    //.then((data) => console.log(data))
+    //.catch((err) => console.log("Error:", err));
+
+});
 
 function initialize(incidents) {
     for(let i = 0; i < 50; ++i) {
-        var newItem = document.createElement("ol")
+        const newItem = document.createElement("li")
         var element = incidents.rows[i];
         var location = element.location_block;
         var date_time = element.dispatch_date_time;
@@ -18,7 +39,7 @@ function initialize(incidents) {
         
         console.log(location, date_time, event);
     
-        newItem.innertext = `${location}`;
+        newItem.textContent = `${location}` + " - " + `${date_time}`;
         apifeed.appendChild(newItem);
     };
 };
